a854b673fa1558c87a3d9595386cb2e38da60861
Taib Khatib <taibkhatib61@gmail.com>
2026-03-23 15:39:57 +0100

    fix: docker compose ingestion processing and serving, feat: delay_detection for testing

--- stop-spark_cluster_2_workers.sh
+++ stop-spark_cluster_2_workers.sh
@@ -1,4 +1,4 @@
 docker container stop spark-master-tcl
 docker container stop spark-history-tcl
-docker container stop spark-worker-a-tcl
-docker container stop spark-worker-b-tcl
+docker container stop spark-worker-tcl-a
+docker container stop spark-worker-tcl-b
